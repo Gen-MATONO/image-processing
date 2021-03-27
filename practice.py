22c627fb2f14fa2930ea1b27b9e5d72e0f1a6cd1
Gen MATONO <matono0921@gmail.com>
2021-03-28 01:23:37 +0900

    test initial program which use tkinter
    I was able to confirm the operation of tkinter

---
+++ practice.py
@@ -0,0 +1,10 @@
+from skimage import io
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+
+img = io.imread('https://yoyoyo-yo.github.io/Gasyori100knock/assets/imori_256x256.png')
+
+plt.imshow(img)
+plt.show()
