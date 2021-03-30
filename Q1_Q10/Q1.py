2308a26e9a4a5aa3f012c6bc832f06bd976ff001
Gen MATONO <matono0921@gmail.com>
2021-03-30 17:41:04 +0900

    creating new directory for answer Q1-Q10
    And, I solved Q1.

---
+++ Q1_Q10/Q1.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage import io
+
+img_original = io.imread('https://raw.githubusercontent.com/Gen-MATONO/Gasyori100knock/master/Question_01_10/imori.jpg')
+
+img = img_original.copy()
+img = img[..., :: -1]
+
+plt.figure()
+ax = plt.subplot()
+ax.imshow(img)
+plt.show()
+
+##############################################
+# ANSWER
+##############################################
+import cv2
+
+# function: BGR -> RGB
+def BGR2RGB(img):
+    b = img[:, :, 0].copy()
+    g = img[:, :, 1].copy()
+    r = img[:, :, 2].copy()
+
+    # RGB > BGR
+    img[:, :, 0] = r
+    img[:, :, 1] = g
+    img[:, :, 2] = b
+
+    return img
+
+# Read image
+img = cv2.imread("imori.jpg")  # There isn't imori.jpg in this directory. So you should alternatively use io from skimage.
+
+# BGR -> RGB
+img = BGR2RGB(img)
+
+# Save result
+cv2.imwrite("out.jpg", img)
+cv2.imshow("result", img)
+cv2.waitKey(0) # Program is going to stop until you type sny key.
+cv2.destroyAllWindows() # Brake all windows
