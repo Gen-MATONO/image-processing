ff9670890f32cd59e60137f635c5cfc804011936
Gen MATONO <matono0921@gmail.com>
2021-07-05 00:26:16 +0900

    I solved Q3, so I added Q3.py.

---
+++ Q1_Q10/Q3.py
@@ -0,0 +1,84 @@
+# **************************************************************
+# --------------------------------------------------------------
+# my code
+# --------------------------------------------------------------
+
+import cv2
+import numpy as np
+
+# make function
+def convert_gray(img):
+    img = img.astype(np.float64)
+    b = img[:, :, 0]
+    g = img[:, :, 1]
+    r = img[:, :, 2]
+    out = 0.2126*r + 0.7152*g + 0.0722*b
+    return out.astype(np.uint8)
+
+def binalization(img):
+    th = 128
+    img[img < th] = 0
+    img[img >= th] = 255
+    return img
+
+# read image
+img_original = cv2.imread('imori.jpg')
+img = img_original.copy()
+
+# convert to gray scale
+img = convert_gray(img)
+# convert to binalization
+img = binalization(img)
+
+
+cv2.imshow('imori', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# --------------------------------------------------------------
+# answer code
+# --------------------------------------------------------------
+# import cv2
+# import numpy as np
+
+# # Gray scale
+# def BGR2GRAY(img):
+# 	b = img[:, :, 0].copy()
+# 	g = img[:, :, 1].copy()
+# 	r = img[:, :, 2].copy()
+
+# 	# Gray scale
+# 	out = 0.2126*r + 0.7152*g + 0.0722*b
+# 	out = out.astype(np.uint8)
+
+# 	return out
+
+# # binalization
+# def binalization(img, th=128):
+# 	img[img < th] = 0  # ※※※
+# 	img[img >= th] = 255 # ※※※
+# 	return img
+# # ※※※
+# # この構文はndarrayの特定の要素を抽出可能な構文である．
+# # ndarray[condition] で条件を満たすものだけを抽出し1次元に平坦化された配列ができる．
+# # ndarray > th（閾値）とすると閾値をtrueとfalseで出来た配列ができる
+# # ndarray[condition] = value とすると，上に書いたような配列でtrueにだけ代入することができる．
+# # （今回の場合condition が ndarray > th であった．）
+
+
+# # Read image
+# img = cv2.imread("imori.jpg").astype(np.float64)
+
+# # Convert to Grayscale
+# out = BGR2GRAY(img)
+
+# # Convert to Binalization
+# out = binalization(out)
+
+# # Save result
+# # cv2.imwrite("out.jpg", out)
+# cv2.imshow("result", out)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+# **************************************************************
--- test.py
+++ test.py
@@ -5,10 +5,18 @@ img_original = cv2.imread("Q1_Q10/imori.jpg")
 
 img = img_original.copy()
 
-h, w, c = img.shape
+img = img[:, :, 1]
+img[img < 128] = 0
+img[img >= 128] = 255
 
-img[:h//2, :w//2] = img[:h//2, :w//2, ::-1]
-
-cv2.imshow('imori', img)
+cv2.imshow('', img)
 cv2.waitKey(0)
-cv2.destroyAllWindows
+cv2.destroyWindow
+
+# h, w, c = img.shape
+
+# img[:h//2, :w//2] = img[:h//2, :w//2, ::-1]
+
+# cv2.imshow('imori', img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows
